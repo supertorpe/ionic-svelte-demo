f20bd20b5d01059c07f00c6460cadabc9c023932
supertorpe <supertorpe@hotmail.com>
2023-03-11 19:26:51 +0100

    upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import sveltePreprocess from 'svelte-preprocess';
 export default defineConfig({
     plugins: [
 		svelte({
-			preprocess: sveltePreprocess()
+			preprocess: [sveltePreprocess({ typescript: true })]
 		})]
 
 });
